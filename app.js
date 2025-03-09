150377f4679f803067e1414d844d927f1e932460
Rachel <hurui.lcy@gmail.com>
2025-03-08 22:00:59 -0700

    Fetching data and finding doc

--- app.js
+++ app.js
@@ -1,14 +1,53 @@
 const express = require('express')
+const { getDb, connectToDb } = require('./db')
+const { ObjectId } = require('mongodb')
 
-//init app &middleware
-const app = express();
-const port = 3000;
+// init app & middleware
+const app = express()
 
-app.listen(port, ()=>{
-  console.log('app listening on port 3000')
+// db connection
+let db
+
+connectToDb((err) => {
+  if(!err){
+    app.listen('3000', () => {
+      console.log('app listening on port 3000')
+    })
+    db = getDb()
+  }
 })
 
-//routes
+// routes
 app.get('/books', (req, res) => {
-  res.json({message: "welcome to the api"})
+  let books = []
+
+  db.collection('books')
+    .find()
+    .sort({author: 1})
+    .forEach(book => books.push(book))
+    .then(() => {
+      res.status(200).json(books)
+    })
+    .catch(() => {
+      res.status(500).json({error: 'Could not fetch the documents'})
+    })
+})
+
+app.get('/books/:id', (req, res) => {
+
+  if (ObjectId.isValid(req.params.id)) {
+
+    db.collection('books')
+      .findOne({_id: new ObjectId(req.params.id)})
+      .then(doc => {
+        res.status(200).json(doc)
+      })
+      .catch(err => {
+        res.status(500).json({error: 'Could not fetch the document'})
+      })
+
+  } else {
+    res.status(500).json({error: 'Could not fetch the document'})
+  }
+
 })
