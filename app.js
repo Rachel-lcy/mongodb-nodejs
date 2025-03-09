cb86c34625aa8d89520b625e9e11898ae5b937fc
Rachel <hurui.lcy@gmail.com>
2025-03-08 19:21:36 -0700

    install mongodb

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 const express = require('express')
+
+//init app &middleware
+const app = express();
+const port = 3000;
+
+app.listen(port, ()=>{
+  console.log('app listening on port 3000')
+})
+
+//routes
+app.get('/books', (req, res) => {
+  res.json({message: "welcome to the api"})
+})
